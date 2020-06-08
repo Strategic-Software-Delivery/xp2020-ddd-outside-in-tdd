bac0115336281d65e2127a3f9e1d6cabf3a5285e
Kenny <Kenny Baas-Schwegler>
2020-06-08 13:55:50 +0200

    Remove test that causes confusion

--- CinemaReservations/Java/CinemaReservationsAcceptanceTests/src/test/java/com/sdd/cinemareservationsacceptancetests/TicketBoothShould.java
+++ CinemaReservations/Java/CinemaReservationsAcceptanceTests/src/test/java/com/sdd/cinemareservationsacceptancetests/TicketBoothShould.java
@@ -36,21 +36,6 @@ public class TicketBoothShould {
         ticketBooth.allocateSeats(new AllocateSeats(showId, partyRequested));
     }
 
-    @Test
-    public void reserve_first_possibility_for_multiple_seats_when_available() throws IOException, NoMovieScreeningFound, TooManyTicketsRequested, NoPossibleAllocationsFound {
-        String showId = "3";
-        int partyRequested = 2;
-
-        MovieScreeningRepository repository =  new StubMovieScreeningRepository();
-        TicketBooth ticketBooth = new TicketBooth(repository);
-
-        SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(showId, partyRequested));
-
-        assertThat(seatsAllocated.getReservedSeats()).hasSize(2);
-        assertThat(seatsAllocated.getReservedSeats().get(0).toString()).isEqualTo("A6");
-        assertThat(seatsAllocated.getReservedSeats().get(1).toString()).isEqualTo("A7");
-    }
-
     @Test(expected = TooManyTicketsRequested.class)
     public void return_TooManyTicketsRequested_when_9_tickets_are_requested() throws IOException, NoMovieScreeningFound, TooManyTicketsRequested, NoPossibleAllocationsFound {
         String showId = "5";
